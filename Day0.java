11dfc8cb0965272e735e1e77755d0c47089e3803
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-05-03 10:23:47 +0800

    Solve Day 5: Loops

--- Day0.java
+++ Day0.java
@@ -15,7 +15,6 @@ public class Day0 {
 		// Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
       
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
         System.out.println(inputString);
     }
 }
--- Day3.java
+++ Day3.java
@@ -1,14 +1,6 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Day3 {
-
-
 
     private static final Scanner scanner = new Scanner(System.in);
 
