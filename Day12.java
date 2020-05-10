2da9ac814c4f086d0e84d5f375e44f09da7d0292
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-05-10 09:32:12 +0800

    Solve Day 12: Inheritance

---
+++ Day12.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here 
+    public Student(String firstName, String lastName, int id, int[] scores) {
+        super(firstName, lastName, id);
+        this.testScores = scores;
+    }
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    char calculate() {
+        char grade = 'T';
+        int sum = 0;
+        int n = testScores.length;
+        
+        for (int i = 0; i < n; i++) {
+            sum = sum + testScores[i];
+        }
+
+        int average = sum / n;
+
+        if (average >= 90) grade = 'O';
+        else if (average >= 80) grade = 'E';
+        else if (average >= 70) grade ='A';
+        else if (average >= 55) grade = 'P';
+        else if (average >= 40) grade = 'D';
+        else if (average < 40) grade = 'T'; 
+
+        return grade;
+    }
+
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
